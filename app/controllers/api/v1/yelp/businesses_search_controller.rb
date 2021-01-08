8058fe387cd2534755bf5ec8c4d4d11bf8b6f06b
John Kim <abcdefghijohnk@gmail.com>
2021-01-07 20:59:01 -0700

    Edit businesses_search_controller
    Co-Authored-By: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

--- app/controllers/api/v1/yelp/businesses_search_controller.rb
+++ app/controllers/api/v1/yelp/businesses_search_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::Yelp::BusinessesSearchController < ApplicationController
-  def index
-    conn = Faraday.new(url: 'https://relocate-08-yelp-microservice.herokuapp.com')
-    response = conn.get("/")
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    render json: parsed
+  def utilities
+    data = BusinessesSearchFacade.utilities_search(params[:location], params[:type])
+    require "pry"; binding.pry
   end
 end
