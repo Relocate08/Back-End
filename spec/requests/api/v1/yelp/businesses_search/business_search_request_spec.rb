59af1883152a188c29808c45176384b9ff4c1d0c
John Kim <abcdefghijohnk@gmail.com>
2021-01-07 20:59:54 -0700

    Update zip code in spec to correspond to route
    Co-Authored-By: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

--- spec/requests/api/v1/yelp/businesses_search/business_search_request_spec.rb
+++ spec/requests/api/v1/yelp/businesses_search/business_search_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Yelp business search by category" do
   it "can return root message from microservice" do
 
-    get '/api/v1/yelp/businesses_search'
+    get '/api/v1/yelp/80211/utilities/electricity'
     expect(response).to be_successful
     json = JSON.parse(response.body, symbolize_names: true)
 
