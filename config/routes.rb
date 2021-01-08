79d9ff5269f09724acc3b6f34e04a17c0ac94987
John Kim <abcdefghijohnk@gmail.com>
2021-01-07 20:58:20 -0700

    Update routes based on new info from microservices
    Co-Authored-By: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :yelp do
-        get '/businesses_search', to: 'businesses_search#index'
+        get '/:location/utilities/:type', to: 'businesses_search#utilities'
       end
     end
   end
