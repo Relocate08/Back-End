39410bff22c28e298c92209ec82d2c49ad9ba6c4
Chris Allbritton <cla.inbox@gmail.com>
2021-01-09 17:40:15 -0500

    Refactor for readability and add recreation_search
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- app/services/businesses_search_service.rb
+++ app/services/businesses_search_service.rb
@@ -1,11 +1,21 @@
 class BusinessesSearchService
   def self.utility_search(location, type)
     response = conn.get("/#{location}/utilities/#{type}")
+    parsed(response)
+  end
+
+  def self.recreation_search(location, type)
+    response = conn.get("/#{location}/recreation/#{type}")
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
+
   def self.conn
     Faraday.new(url: 'https://relocate-08-yelp-microservice.herokuapp.com')
+  end
+
+  def self.parsed(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
