d1a404096a345f6ec74ac3b752f9a554625feff7
Chris Allbritton <cla.inbox@gmail.com>
2021-01-09 17:34:15 -0500

    Refactor test to reflect change from utility to business
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- spec/requests/api/v1/yelp/businesses_search/business_search_request_spec.rb
+++ spec/requests/api/v1/yelp/businesses_search/business_search_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Yelp business search by category" do
+describe "Yelp businesses search by category" do
   it "can return electric companies based on user input" do
 
     get '/api/v1/yelp/80211/utilities/electricity'
@@ -8,104 +8,104 @@ describe "Yelp business search by category" do
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(json).to be_a(Hash)
-    utility_info = json[:data][0]
-    expect(utility_info).to be_a(Hash)
-    expect(utility_info).to have_key(:id)
-    expect(utility_info[:id]).to be_a(String)
-    expect(utility_info).to have_key(:type)
-    expect(utility_info[:type]).to eq("utility")
-    expect(utility_info).to have_key(:attributes)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
 
-    utility_attr = utility_info[:attributes]
-    expect(utility_attr).to have_key(:name)
-    expect(utility_attr[:name]).to be_a(String)
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
 
-    expect(utility_attr).to have_key(:image)
-    expect(utility_attr[:image]).to be_a(String)
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
 
-    expect(utility_attr).to have_key(:url)
-    expect(utility_attr[:url]).to be_a(String)
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
 
-    expect(utility_attr).to have_key(:phone)
-    expect(utility_attr[:phone]).to be_a(String)
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
 
-    expect(utility_attr).to have_key(:distance)
-    expect(utility_attr[:distance]).to be_a(Float)
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
 
-    expect(utility_attr).to have_key(:location)
-    expect(utility_attr[:location]).to be_a(String)
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
   end
 
 
   it "can return water companies based on user input" do
-    get '/api/v1/yelp/80211/utilities/water' 
-    
+    get '/api/v1/yelp/80211/utilities/water'
+
     expect(response).to be_successful
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(json).to be_a(Hash)
-    utility_info = json[:data][0]
-    expect(utility_info).to be_a(Hash)
-    expect(utility_info).to have_key(:id)
-    expect(utility_info[:id]).to be_a(String)
-    expect(utility_info).to have_key(:type)
-    expect(utility_info[:type]).to eq("utility")
-    expect(utility_info).to have_key(:attributes)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
 
-    utility_attr = utility_info[:attributes]
-    expect(utility_attr).to have_key(:name)
-    expect(utility_attr[:name]).to be_a(String)
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
 
-    expect(utility_attr).to have_key(:image)
-    expect(utility_attr[:image]).to be_a(String)
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
 
-    expect(utility_attr).to have_key(:url)
-    expect(utility_attr[:url]).to be_a(String)
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
 
-    expect(utility_attr).to have_key(:phone)
-    expect(utility_attr[:phone]).to be_a(String)
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
 
-    expect(utility_attr).to have_key(:distance)
-    expect(utility_attr[:distance]).to be_a(Float)
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
 
-    expect(utility_attr).to have_key(:location)
-    expect(utility_attr[:location]).to be_a(String)
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
   end
 
   it "can return isp companies based on user input" do
 
     get '/api/v1/yelp/80211/utilities/isps'
-    
+
     expect(response).to be_successful
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(json).to be_a(Hash)
-    utility_info = json[:data][0]
-    expect(utility_info).to be_a(Hash)
-    expect(utility_info).to have_key(:id)
-    expect(utility_info[:id]).to be_a(String)
-    expect(utility_info).to have_key(:type)
-    expect(utility_info[:type]).to eq("utility")
-    expect(utility_info).to have_key(:attributes)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
 
-    utility_attr = utility_info[:attributes]
-    expect(utility_attr).to have_key(:name)
-    expect(utility_attr[:name]).to be_a(String)
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
 
-    expect(utility_attr).to have_key(:image)
-    expect(utility_attr[:image]).to be_a(String)
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
 
-    expect(utility_attr).to have_key(:url)
-    expect(utility_attr[:url]).to be_a(String)
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
 
-    expect(utility_attr).to have_key(:phone)
-    expect(utility_attr[:phone]).to be_a(String)
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
 
-    expect(utility_attr).to have_key(:distance)
-    expect(utility_attr[:distance]).to be_a(Float)
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
 
-    expect(utility_attr).to have_key(:location)
-    expect(utility_attr[:location]).to be_a(String)
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
   end
 
   it "can return tv providers based on user input" do
@@ -117,31 +117,31 @@ describe "Yelp business search by category" do
 
     expect(json).to be_a(Hash)
 
-    utility_info = json[:data][0]
-    expect(utility_info).to be_a(Hash)
-    expect(utility_info).to have_key(:id)
-    expect(utility_info[:id]).to be_a(String)
-    expect(utility_info).to have_key(:type)
-    expect(utility_info[:type]).to eq("utility")
-    expect(utility_info).to have_key(:attributes)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
 
-    utility_attr = utility_info[:attributes]
-    expect(utility_attr).to have_key(:name)
-    expect(utility_attr[:name]).to be_a(String)
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
 
-    expect(utility_attr).to have_key(:image)
-    expect(utility_attr[:image]).to be_a(String)
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
 
-    expect(utility_attr).to have_key(:url)
-    expect(utility_attr[:url]).to be_a(String)
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
 
-    expect(utility_attr).to have_key(:phone)
-    expect(utility_attr[:phone]).to be_a(String)
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
 
-    expect(utility_attr).to have_key(:distance)
-    expect(utility_attr[:distance]).to be_a(Float)
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
 
-    expect(utility_attr).to have_key(:location)
-    expect(utility_attr[:location]).to be_a(String)
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
   end
 end
