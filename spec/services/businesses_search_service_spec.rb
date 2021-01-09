e2c10776182f7b8ffd19482ddd79822421897764
Chris Allbritton <cla.inbox@gmail.com>
2021-01-09 15:19:42 -0500

    Add water service test
    Co-Authored-By: John Kim <abcdefghijohnk@gmail.com>

--- spec/services/businesses_search_service_spec.rb
+++ spec/services/businesses_search_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Business Search Service' do
-  it 'search()' do
+  it 'can search for electric data' do
     electricity = BusinessesSearchService.utility_search('80211', 'electricity')
     expect(electricity).to be_a(Hash)
     electricity_info = electricity[:data][0]
@@ -24,5 +24,30 @@ describe 'Business Search Service' do
 
     expect(electricity_info).to have_key(:location)
     expect(electricity_info[:location]).to be_a(Hash)
+  end
+
+  it 'can search for water data' do
+    water = BusinessesSearchService.utility_search('80211', 'water')
+    expect(water).to be_a(Hash)
+    water_info = water[:data][0]
+    expect(water_info).to be_an(Hash)
+
+    expect(water_info).to have_key(:name)
+    expect(water_info[:name]).to be_a(String)
+
+    expect(water_info).to have_key(:image)
+    expect(water_info[:image]).to be_a(String)
+
+    expect(water_info).to have_key(:url)
+    expect(water_info[:url]).to be_a(String)
+
+    expect(water_info).to have_key(:phone)
+    expect(water_info[:phone]).to be_a(String)
+
+    expect(water_info).to have_key(:distance)
+    expect(water_info[:distance]).to be_a(Float)
+
+    expect(water_info).to have_key(:location)
+    expect(water_info[:location]).to be_a(Hash)
   end
 end
