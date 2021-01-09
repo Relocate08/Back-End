7e77379e8479bf4cc97d771bfb1d3437b38bb605
John Kim <abcdefghijohnk@gmail.com>
2021-01-09 12:12:52 -0700

    Update utilities_search method to exclude all business not open
    Co-Authored-By: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-Authored-By: Chris Allbritton <cla.inbox@gmail.com>
    Co-Authored-By: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    Co-Authored-By: Zach Stearns <64452036+Stearnzy@users.noreply.github.com>

--- app/controllers/api/v1/yelp/businesses_search_controller.rb
+++ app/controllers/api/v1/yelp/businesses_search_controller.rb
@@ -2,8 +2,5 @@ class Api::V1::Yelp::BusinessesSearchController < ApplicationController
   def utilities
     data = BusinessesSearchFacade.utilities_search(params[:location], params[:type])
     render json: UtilitySerializer.new(data)
-    # x = data.each_with_object([]) do |obj, array|
-    #   array << UtilitySerializer.new(obj)
-    # end
   end
 end
--- app/facades/businesses_search_facade.rb
+++ app/facades/businesses_search_facade.rb
@@ -1,8 +1,7 @@
 class BusinessesSearchFacade
   def self.utilities_search(location, type)
     utilities = BusinessesSearchService.utility_search(location, type)
-    utilities[:data].map do |result|
-      Utility.new(result)
-    end
+    open_businesses = utilities[:data].select {|result| result[:is_closed] == false}
+    open_businesses.map{|result| Utility.new(result)}
   end
 end
