d54416dc08de11e9dbc43d354686556ba18483fb
Zach Stearns <zach.stearns92@gmail.com>
2021-01-09 13:10:28 -0700

    Add isps tests
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-authored-by: Christopher Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-authored-by: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- spec/facades/business_search_facade_spec.rb
+++ spec/facades/business_search_facade_spec.rb
@@ -13,4 +13,17 @@ describe 'Businesses Search Facade' do
     expect(utility.first.phone).to be_a(String)
     expect(utility.first.url).to be_a(String)
   end
+
+  it 'can see search results' do
+    utility = BusinessesSearchFacade.utilities_search('80211', 'isps')
+
+    expect(utility).to be_an(Array)
+    expect(utility.first).to be_a(Utility)
+    expect(utility.first.distance).to be_a(Float)
+    expect(utility.first.image).to be_a(String)
+    expect(utility.first.location).to be_a(String)
+    expect(utility.first.name).to be_a(String)
+    expect(utility.first.phone).to be_a(String)
+    expect(utility.first.url).to be_a(String)
+  end
 end
--- spec/services/businesses_search_service_spec.rb
+++ spec/services/businesses_search_service_spec.rb
@@ -25,4 +25,29 @@ describe 'Business Search Service' do
     expect(electricity_info).to have_key(:location)
     expect(electricity_info[:location]).to be_a(Hash)
   end
+
+  it 'search()' do
+    isps = BusinessesSearchService.utility_search('80211', 'isps')
+    expect(isps).to be_a(Hash)
+    isps_info = isps[:data][0]
+    expect(isps_info).to be_an(Hash)
+
+    expect(isps_info).to have_key(:name)
+    expect(isps_info[:name]).to be_a(String)
+
+    expect(isps_info).to have_key(:image)
+    expect(isps_info[:image]).to be_a(String)
+
+    expect(isps_info).to have_key(:url)
+    expect(isps_info[:url]).to be_a(String)
+
+    expect(isps_info).to have_key(:phone)
+    expect(isps_info[:phone]).to be_a(String)
+
+    expect(isps_info).to have_key(:distance)
+    expect(isps_info[:distance]).to be_a(Float)
+
+    expect(isps_info).to have_key(:location)
+    expect(isps_info[:location]).to be_a(Hash)
+  end
 end
