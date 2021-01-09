b81d3d28ecea79ecb79c6bad3daffcbc206e7aad
John Kim <abcdefghijohnk@gmail.com>
2021-01-08 18:18:51 -0700

    Create utility serializer
    Co-Authored-By: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-Authored-By: Hope Gochnour <52016184+hopesgit@users.noreply.github.com>
    Co-Authored-By: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>

--- app/controllers/api/v1/yelp/businesses_search_controller.rb
+++ app/controllers/api/v1/yelp/businesses_search_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::Yelp::BusinessesSearchController < ApplicationController
   def utilities
     data = BusinessesSearchFacade.utilities_search(params[:location], params[:type])
-    require "pry"; binding.pry
+    render json: UtilitySerializer.new(data)
+    # x = data.each_with_object([]) do |obj, array|
+    #   array << UtilitySerializer.new(obj)
+    # end
   end
 end
