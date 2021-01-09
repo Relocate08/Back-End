0e1e8fd252e814f9271f71daa25a5a649bf6ea18
Chris Allbritton <cla.inbox@gmail.com>
2021-01-09 17:39:49 -0500

    Refactor from utility to business
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- spec/facades/business_search_facade_spec.rb
+++ spec/facades/business_search_facade_spec.rb
@@ -4,9 +4,9 @@ describe 'Businesses Search Facade' do
 
   it 'can see electricity search results' do
     utility = BusinessesSearchFacade.utilities_search('80211', 'electricity')
-   
+
     expect(utility).to be_an(Array)
-    expect(utility.first).to be_a(Utility)
+    expect(utility.first).to be_a(Business)
     expect(utility.first.distance).to be_a(Float)
     expect(utility.first.image).to be_a(String)
     expect(utility.first.location).to be_a(String)
@@ -14,13 +14,12 @@ describe 'Businesses Search Facade' do
     expect(utility.first.phone).to be_a(String)
     expect(utility.first.url).to be_a(String)
   end
-
 
   it 'can see water search results' do
     utility = BusinessesSearchFacade.utilities_search('80211', 'water')
-   
+
     expect(utility).to be_an(Array)
-    expect(utility.first).to be_a(Utility)
+    expect(utility.first).to be_a(Business)
     expect(utility.first.distance).to be_a(Float)
     expect(utility.first.image).to be_a(String)
     expect(utility.first.location).to be_a(String)
@@ -28,12 +27,12 @@ describe 'Businesses Search Facade' do
     expect(utility.first.phone).to be_a(String)
     expect(utility.first.url).to be_a(String)
   end
-  
+
   it 'can see isp search results' do
     utility = BusinessesSearchFacade.utilities_search('80211', 'isps')
-    
+
     expect(utility).to be_an(Array)
-    expect(utility.first).to be_a(Utility)
+    expect(utility.first).to be_a(Business)
     expect(utility.first.distance).to be_a(Float)
     expect(utility.first.image).to be_a(String)
     expect(utility.first.location).to be_a(String)
@@ -41,12 +40,12 @@ describe 'Businesses Search Facade' do
     expect(utility.first.phone).to be_a(String)
     expect(utility.first.url).to be_a(String)
   end
-  
+
   it 'can see tv search results' do
     utility = BusinessesSearchFacade.utilities_search('80211', 'tv')
 
     expect(utility).to be_an(Array)
-    expect(utility.first).to be_a(Utility)
+    expect(utility.first).to be_a(Business)
     expect(utility.first.distance).to be_a(Float)
     expect(utility.first.image).to be_a(String)
     expect(utility.first.location).to be_a(String)
