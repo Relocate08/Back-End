f765a1c6a10f98b4512a6b4fe5b015a0680e9487
Chris Allbritton <cla.inbox@gmail.com>
2021-01-11 19:28:44 -0500

    Add hiking recreation pools endpoint tests
    Co-Authored-By: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- spec/requests/api/v1/yelp/businesses_search/business_search_request_spec.rb
+++ spec/requests/api/v1/yelp/businesses_search/business_search_request_spec.rb
@@ -284,4 +284,112 @@ describe "Yelp businesses search by category" do
     expect(businesses_attr).to have_key(:location)
     expect(businesses_attr[:location]).to be_a(String)
   end
+
+  it "can return hiking locations based on user input" do
+
+    get '/api/v1/yelp/80211/recreation/hiking'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to be_a(Hash)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
+
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
+
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
+  end
+
+  it "can return recreation activities based on user input" do
+
+    get '/api/v1/yelp/80211/recreation/recreation'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to be_a(Hash)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
+
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
+
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
+  end
+
+  it "can return pool companies based on user input" do
+
+    get '/api/v1/yelp/80211/recreation/pools'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to be_a(Hash)
+    businesses_info = json[:data][0]
+    expect(businesses_info).to be_a(Hash)
+    expect(businesses_info).to have_key(:id)
+    expect(businesses_info[:id]).to be_a(String)
+    expect(businesses_info).to have_key(:type)
+    expect(businesses_info[:type]).to eq("businesses")
+    expect(businesses_info).to have_key(:attributes)
+
+    businesses_attr = businesses_info[:attributes]
+    expect(businesses_attr).to have_key(:name)
+    expect(businesses_attr[:name]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:image)
+    expect(businesses_attr[:image]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:url)
+    expect(businesses_attr[:url]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:phone)
+    expect(businesses_attr[:phone]).to be_a(String)
+
+    expect(businesses_attr).to have_key(:distance)
+    expect(businesses_attr[:distance]).to be_a(Float)
+
+    expect(businesses_attr).to have_key(:location)
+    expect(businesses_attr[:location]).to be_a(String)
+  end
 end
