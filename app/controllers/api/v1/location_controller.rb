64b779acde66fee85561f665cfe6dc918af1d45c
Zach Stearns <zach.stearns92@gmail.com>
2021-01-11 20:59:21 -0700

    Corrected file location
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

---
+++ app/controllers/api/v1/location_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::LocationController < ApplicationController
+  def show
+    render json: LocationSerializer.new(Location.find(params[:user_id]))
+  end
+  
+  def create
+    render json: LocationSerializer.new(Location.create(location_params))
+  end
+  
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def location_params
+    params.permit(:user_id, :location)
+  end
+end
