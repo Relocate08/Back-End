ca61a05b93cdb1121e198ef5ff7e7ce90b59172c
Jake Heft <jakeheft@gmail.com>
2021-01-13 19:28:03 -0700

    Lint search facade

--- app/facades/businesses_search_facade.rb
+++ app/facades/businesses_search_facade.rb
@@ -1,20 +1,20 @@
 class BusinessesSearchFacade
   def self.utilities_search(location, type)
     businesses = BusinessesSearchService.utility_search(location, type)
-    is_open(businesses)
+    open?(businesses)
   end
 
   def self.recreation_search(location, type)
     businesses = BusinessesSearchService.recreation_search(location, type)
-    is_open(businesses)
+    open?(businesses)
   end
 
   def self.homeservices_search(location, type)
     businesses = BusinessesSearchService.homeservice_search(location, type)
-    is_open(businesses)
+    open?(businesses)
   end
 
-  def self.is_open(businesses)
+  def self.open?(businesses)
     results = businesses[:data].select { |result| result[:is_closed] == false }
     results.map { |result| Business.new(result) }
   end
